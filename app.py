81167407272dc5724f267d3fb49e184aac985f0b
maulanaatthur <maulanaatthur12@gmail.com>
2023-12-08 21:54:30 +0700

    about

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ def home():
         return render_template('index.html')
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/mulaikonsultasi")
+def konsultasi():
+    return render_template('artikel.html')
+
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run('0.0.0.0',port=5000,debug=True)
